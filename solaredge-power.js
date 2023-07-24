88b40ceb31fb57c4dd07352f079768cf4b4232f6
Yannick Simon <yannick.simon@gmail.com>
2023-07-24 20:56:27 +0200

    0.3.3

--- solaredge-power.js
+++ solaredge-power.js
@@ -2,6 +2,25 @@ var https = require('https'),
     urllib = require("url"),
     EventEmitter = require('events').EventEmitter;
 
+function dateFormat (date, fstr, utc) {
+  utc = utc ? 'getUTC' : 'get';
+  return fstr.replace (/%[YmdHMS]/g, function (m) {
+    switch (m) {
+    case '%Y': return date[utc + 'FullYear'] (); // no leading zeros required
+    case '%m': m = 1 + date[utc + 'Month'] (); break;
+    case '%d': m = date[utc + 'Date'] (); break;
+    case '%H': m = date[utc + 'Hours'] (); break;
+    case '%M': m = date[utc + 'Minutes'] (); break;
+    case '%S': m = date[utc + 'Seconds'] (); break;
+    default: return m.slice (1); // unknown code, remove %
+    }
+    // add leading zero if required
+    return ('0' + m).slice (-2);
+  });
+}
+
+
+
 module.exports = function(RED) {
 
     var API_BASE = "https://monitoringapi.solaredge.com";
@@ -62,15 +81,54 @@ module.exports = function(RED) {
         }
         
         function fetchData2nd(node, msg) {
-           var options = { api_key: node.site.apikey };
+		date5minutesAgo = new Date ();
+		date5minutesAgo.setMinutes(date5minutesAgo.getMinutes() -5);
+           var options = { api_key: node.site.apikey,
+				endTime: dateFormat(new Date (), "%Y-%m-%d %H:%M:%S", true),
+				startTime: dateFormat(date5minutesAgo, "%Y-%m-%d %H:%M:%S", true)
+			 };
            var query="";
             for (var key in options) {
-                query += encodeURIComponent(key) + "=" + encodeURIComponent(options[key]);
+                query += "&" + encodeURIComponent(key) + "=" + encodeURIComponent(options[key]);
             }
-            newurl = API_BASE + "/equipment/" + node.site.siteid + "/" + "7401443E-F7" + "/data?startTime=2023-07-24%2011:00:00&endTime=2023-07-24%2013:00:00&" + query;
+		// console.log(Date());
+
+	    var SN = msg.payload.Inventory.inverters[0].SN;
+	    
+            newurl = API_BASE + "/equipment/" + node.site.siteid + "/" + SN + "/data?a=1" + query;
             msg.newnurl = newurl;
-            node.send(msg);
-            node.status({});
+            // console.log(newurl);
+
+ 	    var opts = urllib.parse(newurl);
+            opts.method = "GET";
+
+            var request = https.request(opts, function(res) {
+                res.setEncoding('utf8');
+
+                var msg = {};
+                msg.statusCode = res.statusCode;
+                msg.payload = "";
+
+                res.on("data", function(chunk) {
+                    msg.payload += chunk;
+                    node.log(chunk);
+                });
+
+                res.on("end",function() {
+                    try { msg.payload = JSON.parse(msg.payload); }
+                    catch(e) { node.warn("Invalid JSON returned"); }
+		            // console.log("youhou");
+                    node.send(msg);
+                    node.status({});
+                });
+            });
+
+            request.on("error", function(err) {
+                node.error(err);
+                node.status({ fill: "red", shape: "dot", text: "error" });
+            });
+
+            request.end();
         }
 
 
